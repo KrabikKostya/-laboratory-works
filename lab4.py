e6240549ed492d5db7aa2be39573daa8e99afb0e
KrabikKostya <kostya.krabik.sw@gmail.com>
2022-03-22 10:10:05 +0200

    added lab4.png

--- lab4.py
+++ lab4.py
@@ -1,24 +1,24 @@
 from random import randint
 
-#1
 m = 10
 n = 10
+#1
 s = 0
-slice = 1
+k = 0
 array = [[randint(-1000, 1001) for i in range(m)] for j in range(n)]
-print(array)
 for i in array:
     s += sum(i)
 s /= n*m
-k = 0
 for i in array:
     for j in i:
         if j % s == 0:
             k += 1
+print(array)
 print(k)
 #2
+slices = 1
 for i in array:
     for j in range(n):
-        i[j] = sum(i[0:slice])
-        slice += 1
+        i[j] = sum(i[0:slices])
+        slices += 1
 print(array)
